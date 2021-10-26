25fb6fab662bff23542184392da47051de2e620c
Wasp IX <stemitom@gmail.com>
2021-10-26 04:43:00 +0100

    Dockerfile update

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
 from flask import Flask, render_template, request
-from elasticsearch import Elasticsearch, ElasticsearchException, helpers
+from elasticsearch import Elasticsearch, ElasticsearchException, RequestsHttpConnection
+from elasticsearch import helpers
+import logging
+
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
+
+# es = Elasticsearch(
+#     hosts=[
+#         {
+#             "host": "host.docker.internal", 
+#             "port": 9200
+#         }
+#     ], 
+#     connection_class=RequestsHttpConnection, 
+#     max_retries=30,
+#     retry_on_timeout=True, 
+#     request_timeout=30
+# )
+
+es = Elasticsearch(
+
+)
+
+@app.route("/index")
+def index():
+    documents = list()
+    with open("data/moviedata.json") as f:
+        for f in f.readlines: documents.append(f)
+    data = helpers.bulk(
+        es, documents, index="movies"
+    )
+    return data
+
 
